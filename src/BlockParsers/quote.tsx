737c7322e076f48eff96701bf7285a4577dbef48
Igor K. <igk19@rambler.ru>
2025-05-09 16:47:32 -0700

    Update quote.tsx
    Added alignJustify to QuoteBlock component.

--- src/BlockParsers/quote.tsx
+++ src/BlockParsers/quote.tsx
@@ -15,6 +15,7 @@ export enum QuoteAlignment {
     left = "left",
     center = "center",
     right = "right",
+    justify="justify"
 }
 
 export type QuoteConfig = {
@@ -22,6 +23,7 @@ export type QuoteConfig = {
         alignCenter?: string,
         alignRight?: string,
         alignLeft?: string,
+        alignJustify?: string,
         quote?: string,
         caption?: string,
     }
@@ -32,6 +34,7 @@ const defaultQuoteConfig: QuoteConfig = {
         alignCenter: "text-center",
         alignRight: "text-right",
         alignLeft: "text-left",
+        alignJustify: "text-left",
         quote: "italic text-lg",
         caption: "opacity-85 text-sm"
     }
@@ -48,6 +51,7 @@ const QuoteBlock = ({item, config}: QuoteProps): React.JSX.Element => {
         [QuoteAlignment.left]: currentConfig.classNames.alignLeft,
         [QuoteAlignment.center]: currentConfig.classNames.alignCenter,
         [QuoteAlignment.right]: currentConfig.classNames.alignRight,
+        [QuoteAlignment.justify]: currentConfig.classNames.alignJustify
     }[item.data.alignment]}><span
         className={currentConfig.classNames.quote}>&quot;{item.data.text}&quot;</span><br/><span
         className={currentConfig.classNames.caption}>- {item.data.caption}</span></p>
